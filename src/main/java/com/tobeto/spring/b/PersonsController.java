3b76f06d850b446dea825c21ed91c63197906bfa
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-03 14:42:10 +0300

    stream api

--- src/main/java/com/tobeto/spring/b/PersonsController.java
+++ src/main/java/com/tobeto/spring/b/PersonsController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -23,22 +24,30 @@ public class PersonsController {
     // path => https://localhost:8080/api/persons/30/halit/19
     // query string => https://localhost:8080/api/persons/getById?id=1&name=halit&age=19
     @GetMapping("{id}")
-    public int getById(@PathVariable int id){
-        return id;
+    public Person getById(@PathVariable int id){
+        // Lambda Expressions
+        // Stream API
+        Person person = inMemoryList
+                .stream()
+                // Lambda Expressions
+                .filter((p) -> p.getId() == id) // 1 eleman [ {} ]
+                .findFirst() // {}
+                .orElseThrow();
+        return person;
     }
 
 
     @PostMapping
-    public String add(){
-        return "Tobeto";
+    public void add(@RequestBody Person person)
+    {
+        inMemoryList.add(person);
     }
     @PutMapping
-    public void update(){
+    public void update(@RequestBody Person person){
 
     }
-    @DeleteMapping
-    public void delete(){
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
 
     }
-    //2:20 dersteyiz
 }
