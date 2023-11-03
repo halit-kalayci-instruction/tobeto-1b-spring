119845498f51b0810d849722590d40efd730295c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-03 14:04:45 +0300

    latest

--- src/main/java/com/tobeto/spring/b/PersonsController.java
+++ src/main/java/com/tobeto/spring/b/PersonsController.java
@@ -1,28 +1,44 @@
 package com.tobeto.spring.b;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("api/persons") // Bu controllerın kontrol edeceği alt routeları tanımlamak
 public class PersonsController {
-
+    List<Person> inMemoryList = new ArrayList<>();
     // In Memory DB
     // CRUD => Create,Read,Update,Delete
+
+
     @GetMapping // api/persons
-    public String get(){
-        return "Get";
+    public List<Person> get(){
+        return inMemoryList;
     }
-    @GetMapping("get2") // api/persons/get2
-    public String get2(){
-        return "Get 2";
+    //@GetMapping("getById")
+    // query string, path variable
+    // path => https://localhost:8080/api/persons/30/halit/19
+    // query string => https://localhost:8080/api/persons/getById?id=1&name=halit&age=19
+    @GetMapping("{id}")
+    public int getById(@PathVariable int id){
+        return id;
     }
+
+
     @PostMapping
-    public String post(){
+    public String add(){
         return "Tobeto";
     }
+    @PutMapping
+    public void update(){
+
+    }
+    @DeleteMapping
+    public void delete(){
+
+    }
+    //2:20 dersteyiz
 }
