9b1030207711a5e39b29d6cbf55946cf84f8bce7
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-08 13:38:51 +0300

    global ex. handler v2

--- src/main/java/com/tobeto/spring/b/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tobeto/spring/b/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.tobeto.spring.b.core.utilities.exceptions;
 
+import org.slf4j.event.KeyValuePair;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,7 +20,27 @@ public class GlobalExceptionHandler {
     // type'ını vermek
     @ExceptionHandler({ MethodArgumentNotValidException.class })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationException() {
-        return "Validasyon hatası";
+    public Map<String,String> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String,String> errors = new HashMap<>();
+
+        for (FieldError error: exception.getBindingResult().getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return errors;
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRuntimeError(RuntimeException exception){
+        return exception.getMessage();
+    }
+
+    // Known Types
+    // Unknown types
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUnkownErrors(){
+        return "Bilinmedik hata";
     }
 }
